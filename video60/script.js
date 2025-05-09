7e4c17234bf4ff3177e2cfeae3fba2460de3ec89
Codemaster929 <mrx6909@gmail.com>
2025-05-09 23:26:56 +0530

    added some code

---
+++ video60/script.js
@@ -0,0 +1,32 @@
+console.log("This is string tutorial...")
+// let a ="sahaj";
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+
+// console.log(a.length);
+// let job="web dev";
+// let salary="200K dollars";
+// console.log(`My job is ${job} and my salary is ${salary}`);
+// // here brackets are nedded as uppercase and lowercase are functions  
+// console.log(job.toUpperCase());
+//  console.log(job.toLowerCase());
+//  console.log(job.length);
+//  let x="aerospacetechaero";
+//  console.log(x);
+//  console.log(x.slice(1,6));
+//  console.log(x.slice(4));
+//  console.log(x.slice(4,7));
+//  console.log(x.replace("space","game"));
+//  console.log(x.replace("aero", "water"));
+
+// let a="ramu";
+// let b="sona";
+// console.log(b.concat(a,"maru","surab"));
+let z="  F1 Race  ";
+console.log("before trim length:",z.length);
+let z2=z.trim();
+console.log("after trim length",z2.length);
+
